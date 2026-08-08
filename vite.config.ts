194aed6af93b6e89d355dee33bc79ffb9fe82c6f
tangxianxu54-oss <tangxianxu54@gmail.com>
2026-08-08 14:59:52 +0800

    Add source: vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    sourcemap: 'hidden',
+  },
+  server: {
+    allowedHosts: true,
+  },
+  preview: {
+    allowedHosts: true,
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          'react-dev-locator',
+        ],
+      },
+    }),
+    tsconfigPaths()
+  ],
+})
